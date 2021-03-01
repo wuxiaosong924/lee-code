2f878052e8857fee1226c71992b48bb09f911025
吴林松 <wulinsong@wulinsongdeMacBook-Pro.local>
2021-03-01 10:12:59 +0800

    lee-code 303

---
+++ src/main/java/cn/wulinsong/algorithm/NumArray.java
@@ -0,0 +1,21 @@
+package cn.wulinsong.algorithm;
+
+/**
+ * lee-code 303. 区域和检索 - 数组不可变
+ */
+public class NumArray {
+
+    private int[] sum;
+
+    public NumArray(int[] nums) {
+        int len = nums.length;
+        sum = new int[len + 1];
+        for (int i = 0; i < nums.length; i ++) {
+            sum[i + 1] = nums[i] + sum[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+}
